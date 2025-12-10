d877c5fde6511981a4465473ad50fe91fb9019a6
РомаР <rodovroman@yandex.ru>
2025-12-10 12:52:50 +0300

    Вернул main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import asyncio
+from aiogram import Bot, Dispatcher, types
+
+class CreateBot:
+    def __init__(self):
+        self.bot = Bot(token='')
+
+class User:
+    pass
+
+class GetCards:
+    pass
+
+class Manager:
+    pass
+
+class Notification:
+    pass
